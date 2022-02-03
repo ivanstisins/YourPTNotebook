da2c22563067f62f8415a51ce1aeb25e3e284302
Ivans <ivantishin50@gmail.com>
2022-02-03 16:57:14 +0000

    test

---
+++ app/src/main/java/com/example/yourptnotebook/CreateExercise.java
@@ -0,0 +1,2 @@
+package com.example.yourptnotebook;public class CreateExercise {
+}
---
+++ app/src/main/java/com/example/yourptnotebook/create_workout.java
@@ -0,0 +1,14 @@
+package com.example.yourptnotebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class create_workout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_workout);
+    }
+}
